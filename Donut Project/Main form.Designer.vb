459517719cf7b39b82ca5f6be11eeb9e472858d9
KETEYIAN CADE <KCade2985@slps.org>
2023-04-05 13:51:49 -0500

    gghghg

--- Donut Project/Main form.Designer.vb
+++ Donut Project/Main form.Designer.vb
@@ -23,22 +23,22 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.radChocolate = New System.Windows.Forms.GroupBox()
-        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
-        Me.radGlazed = New System.Windows.Forms.RadioButton()
-        Me.radSugar = New System.Windows.Forms.RadioButton()
-        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
         Me.radFilled = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.radSugar = New System.Windows.Forms.RadioButton()
+        Me.radGlazed = New System.Windows.Forms.RadioButton()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
         Me.radCappuccino = New System.Windows.Forms.RadioButton()
         Me.radRegular = New System.Windows.Forms.RadioButton()
         Me.radNone = New System.Windows.Forms.RadioButton()
         Me.GroupBox3 = New System.Windows.Forms.GroupBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.lblSubtotal = New System.Windows.Forms.Label()
-        Me.lblSalestax = New System.Windows.Forms.Label()
         Me.lblTotal = New System.Windows.Forms.Label()
+        Me.lblSalestax = New System.Windows.Forms.Label()
+        Me.lblSubtotal = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.btnCalc = New System.Windows.Forms.Button()
         Me.btnExit = New System.Windows.Forms.Button()
         Me.radChocolate.SuspendLayout()
@@ -52,65 +52,73 @@ Partial Class Form1
         Me.radChocolate.Controls.Add(Me.RadioButton3)
         Me.radChocolate.Controls.Add(Me.radSugar)
         Me.radChocolate.Controls.Add(Me.radGlazed)
-        Me.radChocolate.Location = New System.Drawing.Point(106, 63)
+        Me.radChocolate.Location = New System.Drawing.Point(80, 51)
+        Me.radChocolate.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.radChocolate.Name = "radChocolate"
-        Me.radChocolate.Size = New System.Drawing.Size(176, 166)
+        Me.radChocolate.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.radChocolate.Size = New System.Drawing.Size(132, 135)
         Me.radChocolate.TabIndex = 0
         Me.radChocolate.TabStop = False
         Me.radChocolate.Text = "Doughnut choices"
         '
-        'radGlazed
+        'radFilled
         '
-        Me.radGlazed.AutoSize = True
-        Me.radGlazed.Location = New System.Drawing.Point(3, 18)
-        Me.radGlazed.Name = "radGlazed"
-        Me.radGlazed.Size = New System.Drawing.Size(113, 20)
-        Me.radGlazed.TabIndex = 0
-        Me.radGlazed.TabStop = True
-        Me.radGlazed.Text = "&Glazed ($1.05)"
-        Me.radGlazed.UseVisualStyleBackColor = True
-        '
-        'radSugar
-        '
-        Me.radSugar.AutoSize = True
-        Me.radSugar.Location = New System.Drawing.Point(3, 58)
-        Me.radSugar.Name = "radSugar"
-        Me.radSugar.Size = New System.Drawing.Size(106, 20)
-        Me.radSugar.TabIndex = 1
-        Me.radSugar.TabStop = True
-        Me.radSugar.Text = "&Sugar ($1.05)"
-        Me.radSugar.UseVisualStyleBackColor = True
+        Me.radFilled.AutoSize = True
+        Me.radFilled.Location = New System.Drawing.Point(4, 114)
+        Me.radFilled.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.radFilled.Name = "radFilled"
+        Me.radFilled.Size = New System.Drawing.Size(85, 17)
+        Me.radFilled.TabIndex = 3
+        Me.radFilled.TabStop = True
+        Me.radFilled.Text = "&Filled ($1.50)"
+        Me.radFilled.UseVisualStyleBackColor = True
         '
         'RadioButton3
         '
         Me.RadioButton3.AutoSize = True
-        Me.RadioButton3.Location = New System.Drawing.Point(6, 99)
+        Me.RadioButton3.Location = New System.Drawing.Point(4, 80)
+        Me.RadioButton3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.RadioButton3.Name = "RadioButton3"
-        Me.RadioButton3.Size = New System.Drawing.Size(131, 20)
+        Me.RadioButton3.Size = New System.Drawing.Size(109, 17)
         Me.RadioButton3.TabIndex = 2
         Me.RadioButton3.TabStop = True
         Me.RadioButton3.Text = "C&hocolate ($1.25)"
         Me.RadioButton3.UseVisualStyleBackColor = True
         '
-        'radFilled
+        'radSugar
         '
-        Me.radFilled.AutoSize = True
-        Me.radFilled.Location = New System.Drawing.Point(6, 140)
-        Me.radFilled.Name = "radFilled"
-        Me.radFilled.Size = New System.Drawing.Size(103, 20)
-        Me.radFilled.TabIndex = 3
-        Me.radFilled.TabStop = True
-        Me.radFilled.Text = "&Filled ($1.50)"
-        Me.radFilled.UseVisualStyleBackColor = True
+        Me.radSugar.AutoSize = True
+        Me.radSugar.Location = New System.Drawing.Point(2, 47)
+        Me.radSugar.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.radSugar.Name = "radSugar"
+        Me.radSugar.Size = New System.Drawing.Size(89, 17)
+        Me.radSugar.TabIndex = 1
+        Me.radSugar.TabStop = True
+        Me.radSugar.Text = "&Sugar ($1.05)"
+        Me.radSugar.UseVisualStyleBackColor = True
+        '
+        'radGlazed
+        '
+        Me.radGlazed.AutoSize = True
+        Me.radGlazed.Location = New System.Drawing.Point(2, 15)
+        Me.radGlazed.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.radGlazed.Name = "radGlazed"
+        Me.radGlazed.Size = New System.Drawing.Size(94, 17)
+        Me.radGlazed.TabIndex = 0
+        Me.radGlazed.TabStop = True
+        Me.radGlazed.Text = "&Glazed ($1.05)"
+        Me.radGlazed.UseVisualStyleBackColor = True
         '
         'GroupBox2
         '
         Me.GroupBox2.Controls.Add(Me.radCappuccino)
         Me.GroupBox2.Controls.Add(Me.radRegular)
         Me.GroupBox2.Controls.Add(Me.radNone)
-        Me.GroupBox2.Location = New System.Drawing.Point(112, 254)
+        Me.GroupBox2.Location = New System.Drawing.Point(84, 206)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(176, 133)
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox2.Size = New System.Drawing.Size(132, 108)
         Me.GroupBox2.TabIndex = 4
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Coffee choices"
@@ -118,9 +126,10 @@ Partial Class Form1
         'radCappuccino
         '
         Me.radCappuccino.AutoSize = True
-        Me.radCappuccino.Location = New System.Drawing.Point(6, 99)
+        Me.radCappuccino.Location = New System.Drawing.Point(4, 80)
+        Me.radCappuccino.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.radCappuccino.Name = "radCappuccino"
-        Me.radCappuccino.Size = New System.Drawing.Size(142, 20)
+        Me.radCappuccino.Size = New System.Drawing.Size(118, 17)
         Me.radCappuccino.TabIndex = 2
         Me.radCappuccino.TabStop = True
         Me.radCappuccino.Text = "Ca&ppuccino ($2.75)"
@@ -129,9 +138,10 @@ Partial Class Form1
         'radRegular
         '
         Me.radRegular.AutoSize = True
-        Me.radRegular.Location = New System.Drawing.Point(3, 58)
+        Me.radRegular.Location = New System.Drawing.Point(2, 47)
+        Me.radRegular.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.radRegular.Name = "radRegular"
-        Me.radRegular.Size = New System.Drawing.Size(118, 20)
+        Me.radRegular.Size = New System.Drawing.Size(98, 17)
         Me.radRegular.TabIndex = 1
         Me.radRegular.TabStop = True
         Me.radRegular.Text = "&Regular ($1.50)"
@@ -140,9 +150,10 @@ Partial Class Form1
         'radNone
         '
         Me.radNone.AutoSize = True
-        Me.radNone.Location = New System.Drawing.Point(3, 18)
+        Me.radNone.Location = New System.Drawing.Point(2, 15)
+        Me.radNone.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.radNone.Name = "radNone"
-        Me.radNone.Size = New System.Drawing.Size(61, 20)
+        Me.radNone.Size = New System.Drawing.Size(51, 17)
         Me.radNone.TabIndex = 0
         Me.radNone.TabStop = True
         Me.radNone.Text = "&None"
@@ -156,91 +167,102 @@ Partial Class Form1
         Me.GroupBox3.Controls.Add(Me.Label3)
         Me.GroupBox3.Controls.Add(Me.Label2)
         Me.GroupBox3.Controls.Add(Me.Label1)
-        Me.GroupBox3.Location = New System.Drawing.Point(321, 71)
+        Me.GroupBox3.Location = New System.Drawing.Point(241, 58)
+        Me.GroupBox3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(200, 158)
+        Me.GroupBox3.Padding = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.GroupBox3.Size = New System.Drawing.Size(150, 128)
         Me.GroupBox3.TabIndex = 3
         Me.GroupBox3.TabStop = False
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(21, 36)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(59, 16)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Subtotal:"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(21, 73)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(65, 16)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Sales tax:"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(21, 113)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(67, 16)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Total due:"
-        '
-        'lblSubtotal
-        '
-        Me.lblSubtotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblSubtotal.Location = New System.Drawing.Point(100, 36)
-        Me.lblSubtotal.Name = "lblSubtotal"
-        Me.lblSubtotal.Size = New System.Drawing.Size(48, 16)
-        Me.lblSubtotal.TabIndex = 3
-        '
-        'lblSalestax
-        '
-        Me.lblSalestax.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblSalestax.Location = New System.Drawing.Point(100, 73)
-        Me.lblSalestax.Name = "lblSalestax"
-        Me.lblSalestax.Size = New System.Drawing.Size(48, 16)
-        Me.lblSalestax.TabIndex = 4
         '
         'lblTotal
         '
         Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.lblTotal.Location = New System.Drawing.Point(100, 113)
+        Me.lblTotal.Location = New System.Drawing.Point(75, 92)
+        Me.lblTotal.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblTotal.Name = "lblTotal"
-        Me.lblTotal.Size = New System.Drawing.Size(48, 16)
+        Me.lblTotal.Size = New System.Drawing.Size(36, 13)
         Me.lblTotal.TabIndex = 5
+        '
+        'lblSalestax
+        '
+        Me.lblSalestax.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblSalestax.Location = New System.Drawing.Point(75, 59)
+        Me.lblSalestax.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblSalestax.Name = "lblSalestax"
+        Me.lblSalestax.Size = New System.Drawing.Size(36, 13)
+        Me.lblSalestax.TabIndex = 4
+        '
+        'lblSubtotal
+        '
+        Me.lblSubtotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblSubtotal.Location = New System.Drawing.Point(75, 29)
+        Me.lblSubtotal.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblSubtotal.Name = "lblSubtotal"
+        Me.lblSubtotal.Size = New System.Drawing.Size(36, 13)
+        Me.lblSubtotal.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 92)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Total due:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 59)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Sales tax:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 29)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Subtotal:"
         '
         'btnCalc
         '
-        Me.btnCalc.Location = New System.Drawing.Point(363, 260)
+        Me.btnCalc.Location = New System.Drawing.Point(302, 211)
+        Me.btnCalc.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnCalc.Name = "btnCalc"
-        Me.btnCalc.Size = New System.Drawing.Size(106, 44)
+        Me.btnCalc.Size = New System.Drawing.Size(80, 36)
         Me.btnCalc.TabIndex = 5
         Me.btnCalc.Text = "&Calculate"
         Me.btnCalc.UseVisualStyleBackColor = True
         '
         'btnExit
         '
-        Me.btnExit.Location = New System.Drawing.Point(363, 329)
+        Me.btnExit.Location = New System.Drawing.Point(272, 267)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.btnExit.Name = "btnExit"
-        Me.btnExit.Size = New System.Drawing.Size(106, 44)
+        Me.btnExit.Size = New System.Drawing.Size(80, 36)
         Me.btnExit.TabIndex = 6
         Me.btnExit.Text = "E&xit"
         Me.btnExit.UseVisualStyleBackColor = True
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(600, 366)
         Me.Controls.Add(Me.btnExit)
         Me.Controls.Add(Me.btnCalc)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.radChocolate)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "Form1"
         Me.Text = "Donut Shoppe"
         Me.radChocolate.ResumeLayout(False)
